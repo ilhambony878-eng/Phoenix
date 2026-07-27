56f13bdfc63129a02ea7bbcaac8ca71465d5a10d
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 01:44:36 +0700

    Update OpenGLContext.cpp

--- src/OpenGLContext.cpp
+++ src/OpenGLContext.cpp
@@ -1,20 +1,34 @@
 #include "OpenGLContext.h"
+
 #include <android/log.h>
 
 #define TAG "PHOENIX"
 
-void OpenGLContext::Init()
+static bool gReady = false;
+
+bool OpenGLContext::Init()
 {
     __android_log_print(
         ANDROID_LOG_INFO,
         TAG,
-        "OpenGL ES Context Initialized");
+        "OpenGLContext Init");
+
+    gReady = true;
+
+    return true;
 }
 
 void OpenGLContext::Shutdown()
 {
+    gReady = false;
+
     __android_log_print(
         ANDROID_LOG_INFO,
         TAG,
-        "OpenGL ES Context Shutdown");
+        "OpenGLContext Shutdown");
+}
+
+bool OpenGLContext::IsReady()
+{
+    return gReady;
 }
