533e73cfba8c0d6efbb6cbb0047b2412d40faaa9
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-27 22:22:34 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
+#include <jni.h>
 #include <android/log.h>
 
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "PHOENIX", __VA_ARGS__)
+#define TAG "PHOENIX"
 
 extern "C"
-void PhoenixInit()
+JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM* vm, void*)
 {
-    LOGI("Phoenix Engine Started!");
+    __android_log_print(
+        ANDROID_LOG_INFO,
+        TAG,
+        "Phoenix Engine Loaded!");
+
+    return JNI_VERSION_1_6;
 }
