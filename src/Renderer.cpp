9a4926d6f837f6b14ffeb83786ae5066e9e30637
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 01:40:39 +0700

    Update Renderer.cpp

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,4 +1,5 @@
 #include "Renderer.h"
+
 #include <android/log.h>
 #include <GLES3/gl3.h>
 
@@ -9,7 +10,24 @@ void Renderer::Init()
     __android_log_print(
         ANDROID_LOG_INFO,
         TAG,
-        "Renderer Initialized");
+        "Renderer Init");
+}
+
+void Renderer::BeginFrame()
+{
+    glClearColor(
+        0.1f,
+        0.1f,
+        0.15f,
+        1.0f);
+
+    glClear(
+        GL_COLOR_BUFFER_BIT |
+        GL_DEPTH_BUFFER_BIT);
+}
+
+void Renderer::EndFrame()
+{
 }
 
 void Renderer::Shutdown()
@@ -18,16 +36,4 @@ void Renderer::Shutdown()
         ANDROID_LOG_INFO,
         TAG,
         "Renderer Shutdown");
-}
-
-void Renderer::BeginFrame()
-{
-}
-
-void Renderer::EndFrame()
-{
-}
-
-void Renderer::Render()
-{
 }
