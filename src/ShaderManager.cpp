0f0ec4eea3751245fcfef0a88a3a121d143b8473
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 00:01:46 +0700

    Update ShaderManager.cpp

--- src/ShaderManager.cpp
+++ src/ShaderManager.cpp
@@ -1,7 +1,10 @@
 #include "ShaderManager.h"
 #include <android/log.h>
+#include <GLES3/gl3.h>
 
 #define TAG "PHOENIX"
+
+static GLuint gShaderProgram = 0;
 
 void ShaderManager::Init()
 {
@@ -14,18 +17,51 @@ void ShaderManager::Init()
 void ShaderManager::Load(const char* vertex,
                          const char* fragment)
 {
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertex, nullptr);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragment, nullptr);
+    glCompileShader(fragmentShader);
+
+    gShaderProgram = glCreateProgram();
+
+    glAttachShader(gShaderProgram, vertexShader);
+    glAttachShader(gShaderProgram, fragmentShader);
+
+    glLinkProgram(gShaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     __android_log_print(
         ANDROID_LOG_INFO,
         TAG,
-        "Shader Loaded");
+        "Shader Compiled Successfully");
 }
 
 void ShaderManager::Use()
 {
+    if (gShaderProgram != 0)
+    {
+        glUseProgram(gShaderProgram);
+    }
+
+    __android_log_print(
+        ANDROID_LOG_INFO,
+        TAG,
+        "Using Shader");
 }
 
 void ShaderManager::Shutdown()
 {
+    if (gShaderProgram != 0)
+    {
+        glDeleteProgram(gShaderProgram);
+        gShaderProgram = 0;
+    }
+
     __android_log_print(
         ANDROID_LOG_INFO,
         TAG,
